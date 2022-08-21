ea5422bbd3d2396e9341984209ed6a278181f291
Bijay Shrestha <7867129+bijay-shrestha@users.noreply.github.com>
2022-08-21 11:59:57 -0500

    Implemented runnable interface

---
+++ src/main/java/com/example/javamultithreading/runnable/ApplicationRunnable.java
@@ -0,0 +1,13 @@
+package com.example.javamultithreading.runnable;
+
+public class ApplicationRunnable implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        ApplicationRunnable runnable = new ApplicationRunnable();
+    }
+}
---
+++ src/test/java/com/example/javamultithreading/TestRunnable.java
@@ -0,0 +1,27 @@
+package com.example.javamultithreading;
+
+import java.io.*;
+
+public class TestRunnable {
+    public static void main(String[] args) {
+         Runnable runnable = ()->{
+             try(BufferedReader reader = new BufferedReader(
+                     new FileReader(
+                             new File("/Users/bijayshrestha/GitHub/Backend/Ex_Files_Java_EE_Concurrency/Exercise Files/Chapter2/02_03/begin/sample.txt")
+                     )
+             )){
+                 String line = null;
+                 while((line = reader.readLine()) != null){
+                     System.out.println(Thread.currentThread().getName() + " reading the line: " + line);
+                 }
+             } catch (FileNotFoundException e) {
+                 throw new RuntimeException(e);
+             } catch (IOException e) {
+                 throw new RuntimeException(e);
+             }
+         };
+
+         Thread thread = new Thread(runnable);
+         thread.start();
+    }
+}
